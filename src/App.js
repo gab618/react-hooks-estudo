c5f892729d1a14c40e6d5bd797497ee9a9b11d36
Gabriel Rocha <knuckles4x@gmail.com>
2020-05-11 17:49:55 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>React Hooks</h1>;
+  const [champions, setChampions] = useState(['Ezreal']);
+  const [newChampion, setNewChampion] = useState('');
+
+  function handleAdd() {
+    setChampions([...champions, newChampion]);
+    setNewChampion('');
+  }
+  return (
+    <>
+      <ul>
+        {champions.map((c) => (
+          <li key={c}>{c}</li>
+        ))}
+      </ul>
+      <input
+        value={newChampion}
+        onChange={(e) => setNewChampion(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </>
+  );
 }
 
 export default App;
